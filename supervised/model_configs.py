40025d3b2b2aa3aa65c1ce2cb0bf06bab6e74c9a
z3bby <z3bbie@gmail.com>
2022-08-23 10:49:44 +0200

    Set supervised encoders to 'baseline'

--- supervised/model_configs.py
+++ supervised/model_configs.py
@@ -3,17 +3,17 @@ import json
 # Note on the configurations: Not all optimizers need all parameters.
 # They are included for easier usage in main.py
 CONFIG_PATH = "./models/model_configs.json"
-CONFIGS = {"Supervised_SGD": {"learning_manager": {"model_name": "Supervised_SGD", "encoder": None},
+CONFIGS = {"Supervised_SGD": {"learning_manager": {"model_name": "Supervised_SGD", "encoder": "baseline"},
                               "training": {"epochs": 10, "batch_size": 32, "optimizer_name": "sgd", "lr": 0.005,
                                            "momentum": 0.7, "weight_decay": 0, "alpha": 0.99, "eps": 1e-08,
                                            "trust_coef": 0.001}},
 
-           "Supervised_RMS": {"learning_manager": {"model_name": "Supervised_RMS", "encoder": None},
+           "Supervised_RMS": {"learning_manager": {"model_name": "Supervised_RMS", "encoder": "baseline"},
                               "training": {"epochs": 10, "batch_size": 32, "optimizer_name": "rmsprop", "lr": 0.002,
                                            "momentum": 0.8, "weight_decay": 0, "alpha": 0.85, "eps": 1.5e-09,
                                            "trust_coef": 0.001}},
 
-           "Supervised_LARS": {"learning_manager": {"model_name": "Supervised_LARS", "encoder": None},
+           "Supervised_LARS": {"learning_manager": {"model_name": "Supervised_LARS", "encoder": "baseline"},
                                "training": {"epochs": 10, "batch_size": 32, "optimizer_name": "lars", "lr": 0.285,
                                             "momentum": 0.3, "weight_decay": 0, "alpha": 0.99, "eps": 1e-08,
                                             "trust_coef": 0.00125}}
